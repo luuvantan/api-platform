48188f5c122e55f814365af12abdc893fd9f0fbe
Luu Van Tan <tan.luuvan@mobifone.vn>
2024-10-01 15:30:20 +0700

    init project

---
+++ app/Models/Book.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use ApiPlatform\Metadata\ApiResource;
+#[ApiResource]
+class Book extends Model
+{
+    use HasFactory;
+}
